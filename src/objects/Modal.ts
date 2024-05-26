615d87b16b296cca80687d9290bc5da68769539b
BrunaDafne <brunadafne467@gmail.com>
2024-05-26 18:49:31 -0300

    criando o input para salvar o nome do usuário no final

--- src/objects/Modal.ts
+++ src/objects/Modal.ts
@@ -1,6 +1,8 @@
 import { addScore } from "src/config/firebase";
 
 export class Modal extends Phaser.GameObjects.Container {
+  private inputElement: HTMLInputElement;
+
   constructor(scene: Phaser.Scene, pontuacao: number, reiniciarCallback: () => void) {
       super(scene);
 
@@ -16,11 +18,17 @@ export class Modal extends Phaser.GameObjects.Container {
       textoPontuacao.setOrigin(0.5);
       this.add(textoPontuacao);
 
+      // Create input field
+      this.createInput(scene);
+
       const botaoSalvar = scene.add.text(scene.sys.canvas.width / 2, scene.sys.canvas.height / 2 + 80, 'Salvar', { fontSize: '24px', color: '#fff' });
       botaoSalvar.setOrigin(0.5);
       botaoSalvar.setInteractive();
       botaoSalvar.on('pointerdown', () => {
-          //reiniciarCallback(); // Fecha o modal antes de chamar addScore
+          const playerName = this.inputElement.value;
+          if (playerName) {
+            addScore(playerName, pontuacao);
+          }
           addScore(`jogador ${pontuacao}`, pontuacao);
       });
       this.add(botaoSalvar);
@@ -28,9 +36,28 @@ export class Modal extends Phaser.GameObjects.Container {
       const botaoReiniciar = scene.add.text(scene.sys.canvas.width / 2, scene.sys.canvas.height / 2 + 50, 'Reiniciar', { fontSize: '24px', color: '#fff' });
       botaoReiniciar.setOrigin(0.5);
       botaoReiniciar.setInteractive();
-      botaoReiniciar.on('pointerdown', reiniciarCallback); // Define o evento de clique para chamar a função de callback
+      botaoReiniciar.on('pointerdown', () => {
+        this.inputElement.remove();
+        reiniciarCallback();
+      }); // Define o evento de clique para chamar a função de callback
       this.add(botaoReiniciar);
 
       scene.add.existing(this);
+  }
+
+  private createInput(scene: Phaser.Scene) {
+    this.inputElement = document.createElement('input');
+    this.inputElement.type = 'text';
+    this.inputElement.placeholder = 'Digite seu nome';
+    this.inputElement.style.position = 'absolute';
+    this.inputElement.style.width = '200px';
+    this.inputElement.style.top = `${scene.sys.canvas.height / 2 - 15}px`;
+    this.inputElement.style.left = `${scene.sys.canvas.width / 2 - 100}px`;
+
+    document.body.appendChild(this.inputElement);
+
+    scene.events.on('shutdown', () => {
+        this.inputElement.remove();
+    });
   }
 }
